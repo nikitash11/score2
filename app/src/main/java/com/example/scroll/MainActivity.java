a9570b4bf85570fe7cf7036afdb4260b5766d5b4
nikitash11 <nikiinika11@gmail.com>
2020-03-20 15:11:29 +0545

    basketball score

--- app/src/main/java/com/example/scroll/MainActivity.java
+++ app/src/main/java/com/example/scroll/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.scroll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG= "MainActivity";
+    public static final String TEAM_A_SCORE= "team_a_score";
 
     private int teamAScore;
     private int teamBScore;
@@ -20,20 +23,61 @@ public class MainActivity extends AppCompatActivity {
 
         textViewTeamA= findViewById(R.id.team_a_score);
         textViewTeamB= findViewById(R.id.team_b_score);
+
+        if(savedInstanceState!=null)
+        {
+            int scoreA= savedInstanceState.getInt(TEAM_A_SCORE);
+            textViewTeamA.setText(""+scoreA);
+        }
+
+        Log.d(TAG, "onCreate");
+
     }
 
-    public void add3ForTeamA(View view)
+    //reset
+    public void resetScore(View view)
     {
-        teamAScore=teamAScore +3;
+        teamAScore=0;
+        teamBScore=0;
         displayScoreForTeamA(teamAScore);
+        displayScoreForTeamB(teamBScore);
     }
 
+    //team B
     public void add3ForTeamB(View view)
     {
         teamBScore=teamBScore +3;
         displayScoreForTeamB(teamBScore);
     }
 
+    public void add2ForTeamB(View view) {
+        teamBScore= teamBScore +2;
+        displayScoreForTeamB(teamBScore);
+    }
+
+    public void add1ForTeamB(View view) {
+        teamBScore= teamBScore+1;
+        displayScoreForTeamB(teamBScore);
+    }
+
+    //team A
+    public void add3ForTeamA(View view)
+    {
+        teamAScore=teamAScore +3;
+        displayScoreForTeamA(teamAScore);
+    }
+
+    public void add2ForTeamA(View view) {
+        teamAScore= teamAScore+2;
+        displayScoreForTeamA(teamAScore);
+    }
+
+    public void add1ForTeamA(View view) {
+        teamAScore= teamAScore+1;
+        displayScoreForTeamA(teamAScore);
+    }
+
+    //display
     public void displayScoreForTeamA(int score)
     {
         textViewTeamA.setText(String.valueOf(score));
@@ -44,12 +88,49 @@ public class MainActivity extends AppCompatActivity {
         textViewTeamB.setText(String.valueOf(score));
     }
 
-    public void resetScore(View view)
-    {
-        teamAScore=0;
-        teamBScore=0;
-        displayScoreForTeamA(teamAScore);
-        displayScoreForTeamB(teamBScore);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+            Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putInt("TEAM_A_SCORE", teamAScore);
     }
 
 
